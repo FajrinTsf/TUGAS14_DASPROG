745e8c25eb9289051ac8b2229413ae1499bb905e
pEGBOL/p <147245065+FajrinTsf@users.noreply.github.com>
2024-06-16 22:11:12 +0700

    Kasir_Fajrin Dasprog14.py

--- tugas14/Kasir_Fajrin Dasprog14.py
+++ tugas14/Kasir_Fajrin Dasprog14.py
@@ -1,3 +1,8 @@
+#NAMA : MUHAMMAD FAJRIN TSINAN F
+#NIM : 20230040086
+#SESI  14 DASAR PEMOGRAMAN
+
+
 barang = input("masukan nama barang :")
 harga = int(input("masukan harga :"))
 jumlah_barang = int(input("masukan jumlah barang :"))
